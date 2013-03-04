db1d9d5ff04e08009f195c87c8e433cceee74508
Kim Griggs <kim.griggs@gmail.com>
2013-03-03 18:17:03 -0800

    migrations

--- db/migrate/20130211063820_devise_create_users.rb
+++ db/migrate/20130211063820_devise_create_users.rb
@@ -4,7 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       ## Database authenticatable
       t.string :email,              :null => false, :default => ""
       t.string :encrypted_password, :null => false, :default => ""
-      t.string  :role, :null => false, :default => "admin"
+      t.string  :role, :null => false, :default => "guest"
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
--- db/seeds.rb
+++ db/seeds.rb
@@ -139,4 +139,4 @@ pd_list.each do |pd|
 end
 
 
-user = User.find_or_create_by_email :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+user = User.find_or_create_by_email :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup, :role => 'admin'
