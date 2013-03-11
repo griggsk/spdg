c0da3d21762df877e74fb5f6300409a37042492c
Kim Griggs <kim.griggs@gmail.com>
2013-03-10 19:31:47 -0700

    layout

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,7 @@
 class Contact < ActiveRecord::Base
   belongs_to :state 
   
-  validates :first, :last, :email,  :presence => true
+  validates :first, :last, :email, :presence => true
   
   def name
     first + " " + last
