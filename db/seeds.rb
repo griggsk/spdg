5f467768c4e8e0c15ec34c4462a90854d106c615
Kim Griggs <kim.griggs@gmail.com>
2013-03-12 19:09:09 -0700

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -152,4 +152,3 @@ pop_list.each do |pop|
   Population.create pop
 end
 
-user = User.find_or_create_by_email :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
