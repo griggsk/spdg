1cfb2cd34144ae725304052a9d1285aaaada6f92
Kim Griggs <kim.griggs@gmail.com>
2013-03-21 21:43:47 -0700

    role

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,6 +42,7 @@ class UsersController < ApplicationController
   #-------------------------------------------------------------------
   def destroy
      authorize! :destroy, @user, :message => 'Not authorized as an administrator.'
+    user = User.find(params[:id]) 
     unless user == current_user
       user.destroy
       redirect_to users_path, :notice => "User deleted."
