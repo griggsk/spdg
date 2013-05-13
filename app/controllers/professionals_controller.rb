f31c222aa250e7f1465f4bb7c7113da0ddf392df
Kim Griggs <kim.griggs@gmail.com>
2013-05-12 19:53:25 -0700

    pd

--- app/controllers/professionals_controller.rb
+++ app/controllers/professionals_controller.rb
@@ -77,7 +77,7 @@ class ProfessionalsController < ApplicationController
     @professional.destroy
 
     respond_to do |format|
-      format.html { redirect_to professional_url }
+      format.html { redirect_to professionals_url }
       format.json { head :no_content }
     end
   end
